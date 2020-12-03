de0a3fc63debc7e2153f53255e3daba81e22261b
yujinchoiii <yujincho@yonsei.ac.kr>
2020-12-04 02:24:30 +0900

    quick hough circle detection

--- app/src/main/java/com/yujinchoi/fpma/MainActivity.kt
+++ app/src/main/java/com/yujinchoi/fpma/MainActivity.kt
@@ -5,27 +5,21 @@ import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.database.Cursor
-import android.graphics.Bitmap
-import android.icu.text.SimpleDateFormat
-import android.net.Uri
-import android.os.Build
+import android.graphics.*
 import android.os.Bundle
-import android.os.Environment
 import android.provider.MediaStore
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import androidx.core.content.FileProvider
-import androidx.core.net.toUri
 import kotlinx.android.synthetic.main.activity_main.*
 import org.opencv.android.OpenCVLoader
 import org.opencv.android.Utils
 import org.opencv.core.*
+import org.opencv.core.Point
 import org.opencv.imgcodecs.Imgcodecs
 import org.opencv.imgproc.Imgproc
 import java.io.File
-import java.io.IOException
-import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
@@ -82,6 +76,7 @@ class MainActivity : AppCompatActivity() {
         intent.type = "image/*"
         val mimeTypes = arrayOf("image/jpeg", "image/png")
         intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes)
+//        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
         startActivityForResult(intent, REQUEST_PICK_IMAGE)
     }
 
@@ -91,8 +86,8 @@ class MainActivity : AppCompatActivity() {
         when (requestCode){
             1 -> {
                 if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
-                    }
                 }
+            }
             11 -> {
                 if (requestCode == REQUEST_PICK_IMAGE && resultCode == Activity.RESULT_OK) {
                     Log.d(TAG, "returned data is ${data?.data?.path}")
@@ -114,35 +109,78 @@ class MainActivity : AppCompatActivity() {
                     Log.d(TAG, "returned image info is ${image}")
                     val colorImage = Mat(image.rows(), image.cols(), CvType.CV_32F)
                     Core.extractChannel(image, colorImage, 0)
-                    val bitmap = Bitmap.createBitmap(
+                    var bitmap = Bitmap.createBitmap(
                         colorImage.cols(),
                         colorImage.rows(),
                         Bitmap.Config.ARGB_8888
                     )
                     Utils.matToBitmap(colorImage, bitmap)
-                    Utils.bitmapToMat(bitmap, colorImage)
+                    bitmap = android.media.ThumbnailUtils.extractThumbnail(bitmap, 400, 300)
+                    val paddedBitmap = Bitmap.createBitmap(800, 700, Bitmap.Config.ARGB_8888)
+                    val can = Canvas(paddedBitmap)
+                    can.drawARGB(Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK) //black padding
+                    can.drawBitmap(bitmap, 200.toFloat(), 200.toFloat(), Paint(Paint.FILTER_BITMAP_FLAG))
+//                    view_img.setImageBitmap(paddedBitmap)
+                    Utils.bitmapToMat(paddedBitmap, colorImage)
                     Imgproc.cvtColor(colorImage, colorImage, Imgproc.COLOR_BGR2GRAY)
                     Imgproc.GaussianBlur(colorImage, colorImage, Size(1.0, 1.0), 0.0)
                     val circles = Mat()
-                    Imgproc.HoughCircles(colorImage, circles, Imgproc.HOUGH_GRADIENT, 1.0, 30.0,
-                        40.0, 30.0, 0, 0)
+                    Imgproc.HoughCircles(
+                        colorImage, circles, Imgproc.HOUGH_GRADIENT, 1.0, 30.0,
+                        30.0, 20.0, 180, 200
+                    )
                     val detected = colorImage.clone()
-                    if (circles.cols()>0) {
+                    if (circles.cols() > 0) {
                         Log.d(TAG, "detected circle info : ${circles.cols()}")
                         for (x in 0 until circles.cols()) {
                             val c1xyr = circles.get(0, x)
-                            val xy = Point(Math.round(c1xyr[0]).toDouble(), Math.round(c1xyr[1]).toDouble())
+                            val xy = Point(
+                                Math.round(c1xyr[0]).toDouble(),
+                                Math.round(c1xyr[1]).toDouble()
+                            )
                             val radius = Math.round(c1xyr[2]).toInt()
                             Log.d(TAG, "detected circle info : $xy , $radius")
                             Imgproc.circle(detected, xy, radius, Scalar(0.0, 0.0, 255.0), 3)
                         }
                     }
-                    Utils.matToBitmap(colorImage, bitmap)
-                    view_img.setImageBitmap(bitmap)
+                    Utils.matToBitmap(colorImage, paddedBitmap)
+                    view_img.setImageBitmap(paddedBitmap)
                 }
 
             }
         }
+    }
+    fun addPadding():Bitmap?{
+        val bitmap = Bitmap.createBitmap(800, 700, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        // draw solid color on full canvas area
+        canvas.drawColor(Color.BLACK)
+
+        // clear color from bitmap drawing area
+        // this is very important for transparent bitmap borders
+        // this will keep bitmap transparency
+        Paint().apply {
+            xfermode = PorterDuffXfermode(PorterDuff.Mode.CLEAR)
+            canvas.drawRect(
+                android.graphics.Rect(
+                    200,
+                    200,
+                    bitmap.width - 200,
+                    bitmap.height - 200
+                ), this
+            )
+        }
+        // finally, draw bitmap on canvas
+        Paint().apply {
+            canvas.drawBitmap(
+                bitmap, // bitmap
+                0f + 200, // left
+                0f + 200, // top
+                this // paint
+            )
+        }
+
+        return bitmap
     }
 
 }
